f747f718f298770b4d1bc3e46fc533064cdb89a0
WILBOARDS <148554800+WILBOARDS@users.noreply.github.com>
2023-10-20 20:06:01 +0700

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wilbert Things</title>
+    <style>
+        *{
+            box-sizing: border-box; 
+        }
+        body{
+            background-color : #303234; 
+            color: yellow;
+        }
+        header {
+            background-color: white;
+            padding: 30px;
+            text-align: center;
+            font-size: 30px;
+            color: black;
+        }
+        footer {
+            background-color: white;
+            padding: 10px;
+            text-align: center;
+            color: white;
+        }
+        h2,h3,h4 {
+            border: 2px solid red ;
+            padding: 30px;
+            font-color: white; 
+            font-family: verdana ;
+            font-size: 160%  ; 
+        }
+    </style>
+</head>
+<body>
+    <header><center><h1>Useful Apps (Wilbert Uses) </h1></center></header>
+    
+    <h2 style="font-size:30px;">Games</h2>
+    
+        <p>1. Steam</br>
+            <ul> Steam created by Valve. Steam is the most popular Game Launcher. There is almost a million games that you can install from Steam. </ul>
+            <ul> <img src="img/steam.jpg" alt="Steam" width="640" height="360"> </ul> </br>
+            <ul> <a href="https://store.steampowered.com/" target="_blank"> Steam Download Link </a> </ul>
+        
+        2. Epic Games </br>
+            <ul> Epic Games is the developer of Fortnite. Epic Games is like steam but there are exclusive games like Fortnite. </ul>
+            <ul> <img src="img/epic games.jpg" alt="Epic Games" width="640" height="360">  </ul> </br>
+            <ul> <a href="https://store.epicgames.com/en-US/download" target="_blank"> Epic Games Link </a> </ul>
+        
+        3. Riot Games </br>
+            <ul> Riot Games is the developer of Valorant, League of Legends, etc. You must install Riot Games Client to play the Popular Valorant Esport Game. </ul>
+            <ul> <img src="img/riot games.jpg" alt="Riot Games" width="640" height="360"> </ul> </br>
+            <ul> <a href="https://www.leagueoflegends.com/en-us/?m=download/" target="_blank"> League Of Legends Installer </a> </ul>
+            <ul> <a href="https://playvalorant.com/id-id/" target="_blank"> Valorant Installer </a> </ul>
+
+        4. Battle.net</br>
+            <ul> Battle.net is created by Blizzard(thats owned by Microsoft now). Battle.net has some Popular Games like Modern Warzone, Diablo Immortal 5, Overwatch Series.</ul>
+            <ul> <img src="img/battle.net.jpg" alt="Battle.net" width="640" height="360"> </ul> </br>
+            <ul> <A href="https://download.battle.net/en-us/desktop" target="_blank"> Battle.net Installer </a> </ul>
+
+    <hr>
+    <h3 style="font-size:30px;"> Video Designing: </h3>
+    
+        <p>1. Davinci Resolve</br>
+            <ul> Davinci Resolve is an Free Advance level Video Designing. It uses a different way of transition,animation by using tabs. </ul>
+            
+        2. ClipChamp</br>  
+            <ul> ClipChamp is made by Microsoft. Its an Entry Level Experience. It has easy export to youtubee, facebook,etc..</ul>
+
+        3. Adobe Premiere</br>
+            <ul> Adobe Premiere is a Paid Advance Level Video Designing Application. It uses the normal layers way for transition, animation. </ul>
+        
+        4. Filmore </br>
+            <ul> Flimore is a Watermark Free Intermediate Level Video Designing Experience. It has everything that you need but the UI is just a bit chaotic. </ul>
+    <hr>
+    <h4 style="font-size:30px;"> Office </h3>
+        
+        <p>1. Microsoft Office </p>
+            <ul> Microsoft Office is the best Office Application for beginners. Microsoft Office is easy-to-use, accesibility, etc.. </ul>
+
+        2. LibreOffice </br>
+            <ul> LibreOffice is the best Free Alternative Office Application. LibreOffice offers similiar or better capabilites of Microsoft Office. </ul>
+ 
+        3. OpenOffice </br>
+            <ul> OpenOffice is a All-In-One Office Application. It uses google's Office Applications for making Presentation, Document, etc..
+    <hr>
+    
+</body>
+</html>
